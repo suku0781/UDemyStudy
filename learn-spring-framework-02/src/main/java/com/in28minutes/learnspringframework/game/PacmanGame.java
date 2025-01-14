9a01bb88da29daded1f1a1838dcb8fd8b1d85467
suku0781 <suku0781@gmail.com>
2025-01-14 13:43:17 +0900

    프로젝트 별 패키징

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,26 @@
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacmanGame implements GamingConsole{
+	
+	public void up() {
+		System.out.println("up");
+	}
+
+	public void down() {
+		System.out.println("down");
+	}
+	
+	public void left() {
+		System.out.println("left");
+	}
+
+	public void right() {
+		System.out.println("right");
+	}
+
+}
